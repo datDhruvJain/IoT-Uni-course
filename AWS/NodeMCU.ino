6e965d87df4bab2df3de23c64a91b8afabde22b0
Dhruv Jain <46486943+datdhruv@users.noreply.github.com>
2022-05-18 01:49:15 +0400

    Create NodeMCU.ino

---
+++ AWS/NodeMCU.ino
@@ -0,0 +1,37 @@
+//Publishing DHT data to database
+#include<DHT.h>
+#include<ESP8266WiFi.h>
+#include<ESP8266HTTPClient.h>
+DHT dht(D1,DHT11);
+float hum,temp_c,temp_f;
+char* ssid = "AndroidAPf3f2";
+char* pass = "mbes0063";
+HTTPClient http;
+WiFiClient client;
+void setup() {
+  Serial.begin(9600);
+  dht.begin();
+  Serial.println("Connecting to WiFi..");
+  WiFi.begin(ssid,pass);
+  while(WiFi.status()!=WL_CONNECTED){
+    Serial.print(".");
+    delay(500);
+    }
+  Serial.println("WiFi Connected");
+}
+void loop() {
+  hum = dht.readHumidity();
+  temp_c = dht.readTemperature();
+  temp_f = dht.readTemperature(true);
+  Serial.print("Humidity:");Serial.print(hum);Serial.print(",");
+  Serial.print("Temperature:");Serial.print(temp_c);Serial.print("*C,");
+  Serial.print(temp_f);Serial.println("*F");
+ 
+  String api = "http://15.185.205.68//insert1.php?h="+String(hum)+"&t="+String(temp_c);
+  http.begin(client, api);
+  int code = http.GET();
+  String response = http.getString();
+  Serial.print("Status Code: ");Serial.println(code);
+  Serial.println(response);
+  delay(5000);
+}
